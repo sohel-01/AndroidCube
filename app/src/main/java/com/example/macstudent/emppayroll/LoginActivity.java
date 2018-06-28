cab2494129ad066be26c9c779bf7f41f7c6847fa
Gagandeep1101 <gagansandhu32@yahoo.com>
2018-06-27 21:08:48 -0400

    First commit

--- app/src/main/java/com/example/macstudent/emppayroll/LoginActivity.java
+++ app/src/main/java/com/example/macstudent/emppayroll/LoginActivity.java
@@ -2,8 +2,20 @@ package com.example.macstudent.emppayroll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edtUserId;
+    private EditText password;
+    private CheckBox chkRememberMe;
+    private Button btnSignIn;
+    private Button btnSignUp;
+    private TextView createAccount;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
